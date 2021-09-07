05d01ea4f790689f5d1a175222e98e2dfd95e8a3
hesham <17p6020@eng.asu.edu.eg>
2021-09-07 13:08:02 +0200

    data

---
+++ lib/data.dart
@@ -0,0 +1,26 @@
+List movies1 = [
+  'dja.jpg',
+  'drive.jpg',
+  'firsy.jpg',
+  'quite.jpg',
+  'seven.jpg',
+  'ter.jpg',
+];
+
+List movies2 = [
+  'baby.jpg',
+  'bas.jpg',
+  'joker.jpg',
+  'mad.jpg',
+  'once.jpg',
+  'warz.jpg',
+];
+
+List movies3 = [
+  'rocky.jpg',
+  'no.jpg',
+  'logan.jpg',
+  'fight.jpeg',
+  'godfather.jpg',
+  'leon.jpg',
+];
